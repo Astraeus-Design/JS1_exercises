287f7964c19c1f46a9318d2ed9bce8f0ad5e9e26
Astraeus-Design <118458240+Astraeus-Design@users.noreply.github.com>
2022-12-10 13:04:57 +0000

    firs commit js1 exercises

---
+++ code.js
@@ -0,0 +1,20 @@
+let age=0;
+age=57;
+console.log(age);
+
+
+let numberOfRabbits = 2;
+numberOfRabbits *=3;
+console.log(numberOfRabbits);
+
+let bodmasExample=3;
+bodmasExample = ((bodmasExample+5)/6);
+console.log(bodmasExample);
+
+let random = Math.floor((Math.random()*6)+1);
+console.log(random);
+
+let myCountry="United Kingdom";
+console.log(myCountry);
+
+console.log(myCountry.length);
